fb8c27036372a6e8adbab334c114518d1308b22a
Aditya Sahu <adityacbsa29@gmail.com>
2026-03-28 02:24:07 +0530

    add routes

--- app/routes/products.py
+++ app/routes/products.py
@@ -3,10 +3,14 @@ from sqlalchemy.orm import Session
 from app.schemas import Product
 import app.models as models
 from app.database import get_db
+from typing import List
+from app.schemas import ProductResponse
+from fastapi import HTTPException
+
 
 router = APIRouter()
 
-@router.post("/products")
+@router.post("/products", response_model=ProductResponse)
 def create_product(product: Product, db: Session = Depends(get_db)):
     
     new_product = models.Product(
@@ -20,3 +24,26 @@ def create_product(product: Product, db: Session = Depends(get_db)):
     db.refresh(new_product)
 
     return {"message": "Product saved"}
+
+
+@router.get("/products",response_model=List[ProductResponse])
+def get_products(db: Session = Depends(get_db)):
+    return db.query(models.product).all()
+
+
+@router.put("/products/{id}",response_model=ProductResponse)
+def update_product(id:int, product:Product,db: Session=Depends(get_db)):
+     existing_product= db.query(models,product).filter(models.product.id == id ).first()
+
+     if not existing_product:
+         raise HTTPException(status_code=404,details= "product not found")
+     
+     existing_product.name=product.name
+     existing_product.price=product.price
+     existing_product.description=product.description
+
+     return existing_product
+
+     
+
+     
