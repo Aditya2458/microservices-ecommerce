7b2837c8d8d1eec998b71a0f019e7e7f2c937591
Aditya Sahu <adityacbsa29@gmail.com>
2026-03-28 02:22:28 +0530

    add products create

--- app/schemas.py
+++ app/schemas.py
@@ -4,3 +4,14 @@ class Product(BaseModel):
     name: str
     price: float
     description: str
+
+
+class ProductResponse(BaseModel):
+    id : str
+    name : str
+    price : float
+    description: str
+
+    class Config:
+        orm_mode=True
+    
